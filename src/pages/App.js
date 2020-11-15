77bb9f9090d51bd1d6acffcc975c60c5cc5ea90d
danikaharu11 <clashhaki55@gmail.com>
2020-11-15 22:13:26 +0800

    Create Nav Component

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../component/Header";
+import Nav from "../component/Nav";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Nav />
     </div>
   );
 }
