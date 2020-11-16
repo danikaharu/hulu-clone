e9e81afb8d966a5e11e17f5a6289762ae01e7508
danikaharu11 <clashhaki55@gmail.com>
2020-11-16 22:28:27 +0800

    Add Nav & Results

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../component/Header";
 import Nav from "../component/Nav";
+import Results from "../component/Results";
 import "./App.css";
+import requests from "../config/requests";
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState(requests.fetchTrending);
+
   return (
     <div className="app">
       <Header />
-      <Nav />
+      <Nav setSelectedOption={setSelectedOption} />
+      <Results selectedOption={selectedOption} />
     </div>
   );
 }
